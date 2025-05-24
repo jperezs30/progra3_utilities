7a105f90bc0c929e6fc7c2f2bbcc77066550b1b2
Fernando Sipaque <josefperez84@gmail.com>
2025-05-23 22:00:55 -0600

    Initializes a basic Java project
    Sets up a Maven project structure with a simple "Hello World!" program as a starting point.

---
+++ src/main/java/com/umg/edu/progra3_utilities/Progra3_utilities.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.umg.edu.progra3_utilities;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Progra3_utilities {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
